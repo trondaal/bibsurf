49c03a932f34e34166ce6277f61d03399f870b38
Martin Nordby Johansen <martinnj@stud.ntnu.no>
2019-03-25 10:50:58 +0100

    #6 added manifestations list, and next page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import WorksList from './containers/ResultContainers/WorksList'
 import ManifestationsList from './containers/ResultContainers/ManifestationsList'
-import CategoryList from './containers/CategoryList'
+
 
 class App extends Component {
   render() {
@@ -11,7 +11,7 @@ class App extends Component {
         <div className='nav-bar'><h2>Her kommer søkebaren</h2></div>
         <div className='results-container'>
           <div className='filter-container'>
-            <CategoryList />
+            hei
           </div>
           <WorksList />
         </div>
