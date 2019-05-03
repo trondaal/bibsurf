174afe406a235ab7d7deb340eb129de20ab59daa
Håvard Bergheim Olsen <haavard.b.olsen@gmail.com>
2019-05-03 16:57:33 +0200

    use MUI button

--- src/components/SideBarFilter/List.js
+++ src/components/SideBarFilter/List.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {clearFilters} from '../../actions'
 
 import {FilterCard, FilterOption} from '.'
+import {Button} from '@material-ui/core'
 
 
 class FilterList extends Component {
@@ -19,7 +20,7 @@ class FilterList extends Component {
         <FilterOption title='Filter options' options={{AND: '', OR: '', ANDOR: ''}} url={this.props.url} />
         {categories && Object.entries(categories).map((e,i) => <FilterCard key={i} title={e[0]} options={e[1]} filterType='categories' url={this.props.url} />)}
         {roles && Object.entries(roles).map((e,i) => <FilterCard key={i} title={e[0]} options={e[1]} filterType='roles' url={this.props.url} />)}
-        <button onClick={this.clearFilters}>Clear Filters</button>
+        <Button style={{marginTop: 8, backgroundColor: "#edeeef"}} variant={"contained"} onClick={this.clearFilters}>Clear Filters</Button>
       </div>
     )
   }
